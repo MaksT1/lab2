e88956ecabb598837d6fde865442b534c9c56f44
MaksT1 <makstkaczyk01@gmail.com>
2026-04-21 19:29:20 +0200

    zad1*

--- lab3/lab3_zad1.java
+++ lab3/lab3_zad1.java
@@ -1,12 +1,21 @@
 package lab3;
 
+enum RodzajSilnika {
+    Benzyna, Diesel, LPG, EV
+}
+
 abstract class Maszyna{
     protected String marka;
     protected String nazwa;
     protected double pojemnoscSilnika;
-    protected enum rodzajSilnika {
-        Benzyna, Diesel, LPG, EV
+    protected RodzajSilnika rodzajSilnika;
+    public Maszyna(String marka, String nazwa, double pojemnoscSilnika, RodzajSilnika rodzajSilnika){
+        this.marka = marka;
+        this.nazwa = nazwa;
+        this.pojemnoscSilnika = pojemnoscSilnika;
+        this.rodzajSilnika = rodzajSilnika;
     }
+
 }
 
 public class lab3_zad1 {
