d85d05b99f4721d97dea44c4e233bd1dbfd543d6
MaksT1 <makstkaczyk01@gmail.com>
2026-04-21 15:13:48 +0200

    zad1

---
+++ lab3/lab3_zad1.java
@@ -0,0 +1,13 @@
+package lab3;
+
+abstract class Maszyna{
+    protected String marka;
+    protected String nazwa;
+    protected double pojemnoscSilnika;
+    protected enum rodzajSilnika {
+        Benzyna, Diesel, LPG, EV
+    }
+}
+
+public class lab3_zad1 {
+}
