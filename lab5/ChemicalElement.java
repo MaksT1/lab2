32b5b2fc779d290bdb979b7745a60da57ad6904f
MaksT1 <makstkaczyk01@gmail.com>
2026-05-21 12:55:37 +0200

    zad6 do konca

--- lab5/ChemicalElement.java
+++ lab5/ChemicalElement.java
@@ -19,5 +19,51 @@ public class ChemicalElement {
     public int getAtomicNumber(){
         return atomicNumber;
     }
+    public boolean isAlkali(){
+        int[] alkali = {3, 11, 19, 37, 55, 87};
+        for(int num : alkali){
+            if(this.atomicNumber == num){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isTransition(){
+        return (this.atomicNumber >=21 && this.atomicNumber <=31) ||
+                (atomicNumber >= 39 && atomicNumber <= 48) ||
+                (atomicNumber >= 72 && atomicNumber <= 80) ||
+                (atomicNumber >= 104 && atomicNumber <= 112);
+    }
+    public boolean isMetal(){
+        int[] metal = {13, 49, 50, 81, 82, 83, 113, 114, 115, 116};
+        for(int num : metal){
+            if(this.atomicNumber == num){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        String type;
+        if(isAlkali()){
+            type = "Alkali metal";
+        }
+        else if(isTransition()){
+            type = "Transition metal";
+        }
+        else if(isMetal()){
+            type = "Metal";
+        }else{
+            type = "Not a metal";
+        }
+        return "Name: " + name + ", Symbol: " + symbol + ", Atomic number: " + atomicNumber + ", Type: " + type;
+    }
+
+    public static void main(String[] args){
+        ChemicalElement chem = new ChemicalElement("Hydrogen", "H", 1);
+        System.out.println(chem.toString());
+    }
 
 }
